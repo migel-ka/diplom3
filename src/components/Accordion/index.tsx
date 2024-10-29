731ea97ed3cf49fe0611ab7c834e4639f01cee9b
Mikhail Sidorov <mu_sidorov@mail.ru>
2024-10-29 22:07:41 +0300

    исправлен слайдер

--- src/components/Accordion/index.tsx
+++ src/components/Accordion/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { AccordionItem } from "./Accordionitem";
+import { AccordionItem } from "./Accordionitem"; 
 import "./index.css";
 
 interface FAQItem {
@@ -12,15 +12,20 @@ interface AccordionProps {
 }
 
 export const Accordion: React.FC<AccordionProps> = ({ faqList }) => {
-  const [openId, setId] = useState<number | null>(null);
+  const [openIds, setOpenIds] = useState<number[]>([]);
+  const toggleItem = (id: number) => {
+    setOpenIds((prev) =>
+      prev.includes(id) ? prev.filter((openId) => openId !== id) : [...prev, id]
+    );
+  };
 
   return (
     <ul className="accordion">
       {faqList.map((faqItem, id) => (
         <AccordionItem
-          onClick={() => (id === openId ? setId(null) : setId(id))}
+          onClick={() => toggleItem(id)}
           faqItem={faqItem}
-          isOpen={id === openId}
+          isOpen={openIds.includes(id)}
           key={id}
         />
       ))}
